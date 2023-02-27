77c96800f2201fd4adfee71e7bbe9ee366752eda
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2023-02-27 22:46:33 +0100

    post model

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'accounts/home.html')
 # Create your views here.
+
 
 def sign_up(request):
     if request.method == "POST":
