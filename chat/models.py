21d3cf5d5c669e3ca5443dd9be0cc25ae43ee3d6
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2023-02-28 18:40:32 +0100

    post form and bug fixing

--- chat/models.py
+++ chat/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    description = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title + '\n' + self.description
