837857e6ece07aa44034aedfdd646f90886b7336
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2023-03-02 23:57:30 +0100

    control push

--- chat/models.py
+++ chat/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title + '\n' + self.description
 
